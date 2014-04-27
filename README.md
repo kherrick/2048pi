52e8214c6133348a830fdfefa76552ce6737b5df
Karl Herrick <me@karlherrick.com>
2014-04-27 19:22:54 -0400

    Fork 2048, use hammer.js, swap swipe code, remove scss

---
+++ README.md
@@ -0,0 +1,3 @@
+#2048pi
+
+A fork of [2048](https://github.com/gabrielecirulli/2048) for the [Raspberry Pi](http://www.raspberrypi.org/), to be utilized on an [Adafruit 320x240 2.8" TFT+Touchscreen](http://adafru.it/1601), utilizing [Hammer.js](https://github.com/EightMedia/hammer.js).
---
+++ js/keyboard_input_manager.js
@@ -0,0 +1,105 @@
+function KeyboardInputManager() {
+  this.events = {};
+
+  this.listen();
+}
+
+KeyboardInputManager.prototype.on = function (event, callback) {
+  if (!this.events[event]) {
+    this.events[event] = [];
+  }
+  this.events[event].push(callback);
+};
+
+KeyboardInputManager.prototype.emit = function (event, data) {
+  var callbacks = this.events[event];
+  if (callbacks) {
+    callbacks.forEach(function (callback) {
+      callback(data);
+    });
+  }
+};
+
+KeyboardInputManager.prototype.listen = function () {
+  var self = this;
+
+  var map = {
+    38: 0, // Up
+    39: 1, // Right
+    40: 2, // Down
+    37: 3, // Left
+    75: 0, // Vim up
+    76: 1, // Vim right
+    74: 2, // Vim down
+    72: 3, // Vim left
+    87: 0, // W
+    68: 1, // D
+    83: 2, // S
+    65: 3  // A
+  };
+
+  // Respond to direction keys
+  document.addEventListener("keydown", function (event) {
+    var modifiers = event.altKey || event.ctrlKey || event.metaKey ||
+                    event.shiftKey;
+    var mapped    = map[event.which];
+
+    if (!modifiers) {
+      if (mapped !== undefined) {
+        event.preventDefault();
+        self.emit("move", mapped);
+      }
+    }
+
+    // R key restarts the game
+    if (!modifiers && event.which === 82) {
+      self.restart.call(self, event);
+    }
+  });
+
+  // Respond to button presses
+  this.bindButtonPress(".retry-button", this.restart);
+  this.bindButtonPress(".restart-button", this.restart);
+  this.bindButtonPress(".keep-playing-button", this.keepPlaying);
+
+  // Respond to swipe events
+  var game  = document.getElementsByClassName("game-container")[0];
+  var swipe = new Hammer(game, {
+    preventDefault: true,
+    swipeVelocityX: 0,
+    swipeVelocityY: 0
+  });
+
+  swipe.on("swipedown swipeup swipeleft swiperight", function(ev){
+    switch (ev.type) {
+      case "swipeup":
+        self.emit("move", 0);
+        break;
+      case "swiperight":
+        self.emit("move", 1);
+        break;
+      case "swipedown":
+        self.emit("move", 2);
+        break;
+      case "swipeleft":
+        self.emit("move", 3);
+        break;
+    }
+  });
+};
+
+KeyboardInputManager.prototype.restart = function (event) {
+  event.preventDefault();
+  this.emit("restart");
+};
+
+KeyboardInputManager.prototype.keepPlaying = function (event) {
+  event.preventDefault();
+  this.emit("keepPlaying");
+};
+
+KeyboardInputManager.prototype.bindButtonPress = function (selector, fn) {
+  var button = document.querySelector(selector);
+  button.addEventListener("click", fn.bind(this));
+  button.addEventListener(this.eventTouchend, fn.bind(this));
+};
